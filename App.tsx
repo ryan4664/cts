3ffd176e13bd0095a5721a9b90fd2350302fb242
Ryan <ryan@rdonohue.ca>
2021-03-02 20:46:42 -0600

    init

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from "react";
+import { SafeAreaView } from "react-native";
+import { Div, Text, ThemeProvider } from "react-native-magnus";
+export default class App extends React.Component {
+  render() {
+    return (
+      <ThemeProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Div alignItems="center" justifyContent="center" flex={1}>
+            <Text
+              fontSize={90}
+              fontWeight="bold"
+              color="red400"
+              letterSpacing={2}
+              mt="lg"
+            >
+              Fuck yeah Ryan
+            </Text>
+          </Div>
+        </SafeAreaView>
+      </ThemeProvider>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
